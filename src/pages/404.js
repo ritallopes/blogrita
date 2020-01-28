461d2957f83c85cb669db681b8e6e7429b90771a
Ritalopes <ritalopes99@gmail.com>
2020-01-28 13:56:49 -0300

    commit with configurations

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+    <h1>404</h1>
+    <p>Endereço errado :/</p>
+  </Layout>
+)
+
+export default NotFoundPage
